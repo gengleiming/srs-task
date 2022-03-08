c4b49b51566ae23a0045f2ac046b3ca54ac72569
gengleiming <geng.leiming@intellif.com>
2022-03-08 14:03:21 +0800

    apollo

--- src/main/java/com/intellif/vesionbook/VesionbookSrsTaskApplication.java
+++ src/main/java/com/intellif/vesionbook/VesionbookSrsTaskApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableApolloConfig({"local", "application"})
+@EnableApolloConfig
+//@EnableApolloConfig({"local", "application"})
 @EnableScheduling
 @EnableFeignClients
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
